c8d10a68dfab785ff6fc9f9c2dcc4dbe0b5d3f2e
RAPHAEL REIS <RAPHAEL_REIS@jabil.com>
2023-11-22 08:49:40 -0300

    Primeiro commit Brazil-map

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { NorthComponent } from './regions/north/north.component';
+import { HomeComponent } from './nav/home/home.component';
+import { NortheastComponent } from './regions/northeast/northeast.component';
+import { CentralwestComponent } from './regions/centralwest/centralwest.component';
+import { SouthComponent } from './regions/south/south.component';
+import { SoutheastComponent } from './regions/southeast/southeast.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'north', component: NorthComponent },
+  { path: 'northeast', component: NortheastComponent },
+  { path: 'centralwest', component: CentralwestComponent },
+  { path: 'southeast', component: SoutheastComponent },
+  { path: 'south', component: SouthComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
